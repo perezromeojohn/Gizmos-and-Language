d2ee08495157775a90e695e214176bc263aa9f95
Romeo John Perez <78357868+perezromeojohn@users.noreply.github.com>
2023-01-11 05:14:36 +0800

    ver 1.4 Added Loading Screen and New Title Screen
    - Added Async loading script for performances of each scene
    - Added new scene particles
    - New Main Menu
    
    * Lastly Implement the UI function and coding analysis as well as the storyline

---
+++ Assets/Scripts/Primary/MainMenuCamera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject boundsGO;
+    private Vector3 direction;
+    private Vector3 lastDirection;
+    private Bounds bounds;
+    
+    void Start()
+    {
+        bounds = boundsGO.GetComponent<Collider>().bounds;
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+        lastDirection = direction;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newPos = transform.position + direction * speed * Time.fixedDeltaTime;
+        if (!bounds.Contains(newPos))
+        {
+            if (newPos.x < bounds.min.x || newPos.x > bounds.max.x) 
+            {
+                direction.x = -direction.x;
+                direction.y = Random.Range(-1f, 1f);
+            }
+            if (newPos.y < bounds.min.y || newPos.y > bounds.max.y) 
+            {
+                direction.y = -direction.y;
+                direction.x = Random.Range(-1f, 1f);
+            }
+        }
+        lastDirection = direction;
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Utility/LoadingScene.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+
+public class LoadingScene : MonoBehaviour
+{
+    public static LoadingScene instance;
+    [SerializeField] private string sceneName;
+    [SerializeField] private GameObject transitionBox;
+    [SerializeField] private GameObject loadingGraphic;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        LoadScene();
+    }
+
+    public async void LoadScene() {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        transitionBox.SetActive(true);
+        loadingGraphic.SetActive(true);
+
+        do {
+            await Task.Delay(5000);
+            Debug.Log("Loading progress: " + scene.progress);
+        } while (scene.progress < 0.9f);
+        scene.allowSceneActivation = true;
+
+        LeanTween.scale(transitionBox, new Vector3(0, 0, 0), 0.6f).setEase(LeanTweenType.easeInOutExpo).setOnComplete(() => {
+            transitionBox.gameObject.SetActive(false);
+        });
+
+        LeanTween.scale(loadingGraphic, new Vector3(0, 0, 0), 0.6f).setEase(LeanTweenType.easeInOutExpo).setOnComplete(() => {
+            loadingGraphic.gameObject.SetActive(false);
+        });
+        
+        Debug.Log("Scene loaded");
+    }
+    
+}
