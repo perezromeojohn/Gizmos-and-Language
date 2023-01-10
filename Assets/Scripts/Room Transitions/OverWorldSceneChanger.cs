cfc9ae7c04aa5bd0588f2a6f906da3397621cbf3
Romeo John Perez <78357868+perezromeojohn@users.noreply.github.com>
2023-01-10 19:12:35 +0800

    patch Pre Global Time Variables
    - A checkpoint patch if the later global time is not working
    - Return to this patch if the following code messes up

--- Assets/Scripts/Room Transitions/OverWorldSceneChanger.cs
+++ Assets/Scripts/Room Transitions/OverWorldSceneChanger.cs
@@ -5,19 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class OverWorldSceneChanger : MonoBehaviour
 {
+
+    [SerializeField] private RectTransform transitionSphere;
     public Vector2 position; // position to move player to in next scene
     public string sceneName; // name of next scene to load
+
+    private void Start() {
+        transitionSphere.gameObject.SetActive(true);
+
+        LeanTween.scale(transitionSphere, new Vector3(1, 1, 1), 0f);
+        LeanTween.scale(transitionSphere, new Vector3(0, 0, 0), 0.6f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => {
+            transitionSphere.gameObject.SetActive(false);
+        });
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            transitionSphere.gameObject.SetActive(true);
             // save player position for next scene
             PlayerPrefs.SetFloat("playerX", position.x);
             PlayerPrefs.SetFloat("playerY", position.y);
 
-            // load next scene
-            SceneManager.LoadScene(sceneName);
+            LeanTween.scale(transitionSphere, new Vector3(0, 0, 0), 0f);
+            LeanTween.scale(transitionSphere, new Vector3(1, 1, 1), 0.6f).setEase(LeanTweenType.easeInQuad).setOnComplete(() => {
+                SceneManager.LoadScene(sceneName);
+            });
         }
     }
 }
--- Assets/Scripts/Room Transitions/SceneChanger.cs
+++ Assets/Scripts/Room Transitions/SceneChanger.cs
@@ -6,12 +6,26 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     public string sceneName;
+    [SerializeField] private RectTransform transitionSphere;
+
+    private void Start() {
+        transitionSphere.gameObject.SetActive(true);
+
+        LeanTween.scale(transitionSphere, new Vector3(1, 1, 1), 0f);
+        LeanTween.scale(transitionSphere, new Vector3(0, 0, 0), 0.6f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => {
+            transitionSphere.gameObject.SetActive(false);
+        });
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            transitionSphere.gameObject.SetActive(true);
+            LeanTween.scale(transitionSphere, new Vector3(0, 0, 0), 0f);
+            LeanTween.scale(transitionSphere, new Vector3(1, 1, 1), 0.6f).setEase(LeanTweenType.easeInQuad).setOnComplete(() => {
+                SceneManager.LoadScene(sceneName);
+            });
         }
     }
 }
